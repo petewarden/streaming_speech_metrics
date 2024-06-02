385274331b8137e367330054bc6c3f297564f207
Pete Warden <pete@usefulsensors.com>
2024-06-02 14:29:33 -0700

    Work on Coqui STT support

--- download_datasets.sh
+++ download_datasets.sh
@@ -11,3 +11,7 @@ unzip -q LibriSpeech-Alignments.zip
 rm -rf LibriSpeech-Alignments.zip
 
 rm -rf unaligned.txt
+
+mkdir -p cache
+curl -L https://github.com/coqui-ai/STT-models/releases/download/english/coqui/v1.0.0-large-vocab/model.tflite -o cache/large-model.tflite
+curl -L https://github.com/coqui-ai/STT-models/releases/download/english%2Fcoqui%2Fv1.0.0-large-vocab/large_vocabulary.scorer -o cache/large-vocabulary.scorer
